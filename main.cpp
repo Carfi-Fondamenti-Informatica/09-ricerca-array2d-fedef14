21c836d73441adfe6204d5a4782d55f25e6c9329
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-13 12:42:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char d2[10][20];
+    char d1[10];
+    int c = 1;
+  
+  for(int i=0;i<10;i++){
+        cin >> d2[i];
+    }
+  cin >> d1;
+ 
+  if(nome(d2,d1,c)){
+        cout << c << endl;
+    } else{
+        cout << "non presente" << endl;
+    }
   return 0;
 }
