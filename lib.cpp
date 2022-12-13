7b54692de5c1c92ebbfc8267493f19285fdf2e99
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-13 19:06:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,9 @@
 #include "lib.h"
 
-int nome(char d2[10][20], char d1[10], int &c){
+int nome(char a[10][20], char d1[10], int &c){
     for(int i=0;i<20;i++){
             for(int j=0;j<10;j++){
-                if(d2[j][i] == d1[i]){
+                if(a[j][i] == d1[i]){
             return c;
                 } else{
                     c++;
