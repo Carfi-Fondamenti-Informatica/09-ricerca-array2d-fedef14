b49bd2b035a7fd6d5c884bebc7dd4762fa65a468
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-13 19:06:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main(){
-  char d2[10][20];
+  char a[10][20];
     char d1[10];
     int c = 0;
   
   for(int i=0;i<10;i++){
-        cin >> d2[i];
+        cin >> a[i];
     }
   cin >> d1;
  
-  if(nome(d2,d1,c)<10){
+  if(nome(a,d1,c)<10){
         cout << c << endl;
     } else{
         cout << "non presente" << endl;
