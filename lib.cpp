8622d8fa20d99c86e6607f9aa53317162183be98
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-13 13:14:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -13,7 +13,9 @@ bool nome(char d2[10][20], char d1[10], int &c){
                 }
 
             }
-            return false;
+            if(c>=10){
+                return false;
+    }
     }
 }
 }
