57188fe19165f357d152f91714faac20d91a9738
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-13 14:13:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,9 +2,6 @@
 
 bool nome(char d2[10][20], char d1[10], int &c){
     for(int i=0;i<20;i++){
-        if(d2[i] == d1){
-            return true;
-        } else{
             for(int j=0;j<10;j++){
                 if(d2[j][i] == d1[i]){
             return c;
@@ -17,7 +14,6 @@ bool nome(char d2[10][20], char d1[10], int &c){
                 return false;
     }
     }
-}
 }
 
 
