e241a2c1964b3a941c8d363926fb40ee9e6adf01
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-13 13:04:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,6 @@
 #include "lib.h"
 
-int nome(char d2[10][20], char d1[10], int &c){
+bool nome(char d2[10][20], char d1[10], int &c){
     for(int i=0;i<20;i++){
         if(d2[i] == d1){
             return true;
