4f871f421377d79335f4110e8d938d580c29e90b
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-15 22:05:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,7 @@
 bool nome(char a[10][20], char d1[20], int &i){
     for(int k=0;k<10;k++){
             for(int j=0;j<20;j++){
-                if(a[k][j] =! d1[j]){
+                if(a[k][j] != d1[j]){
             j=20;
                 } else if(j == 19){
                     i = k;
