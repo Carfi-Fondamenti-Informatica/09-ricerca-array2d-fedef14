88ae3f4346af4d2f8361c1ad4fdfc858f2dc9a87
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-15 21:59:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,6 @@
 #include "lib.h"
 
-int nome(char a[10][20], char d1[20], int &i){
+bool nome(char a[10][20], char d1[20], int &i){
     for(int k=0;k<10;k++){
             for(int j=0;j<20;j++){
                 if(a[k][j] =! d1[j]){
